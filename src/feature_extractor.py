ce091c5a6bd3bc9cd380ed479b21c7cdb26b3ab8
Theresa Kocher <the.kocher@gmx.de>
2019-04-16 07:23:18 +0200

    fix: Fix pickle read and write for all platforms

--- src/feature_extractor.py
+++ src/feature_extractor.py
@@ -308,29 +308,37 @@ def save_as_pickled_object(obj, filepath):
     """
     This is a defensive way to write pickle.write, allowing for very large files on all platforms
     """
-    max_bytes = 2**31 - 1
-    bytes_out = pickle.dumps(obj)
-    n_bytes = sys.getsizeof(bytes_out)
-    with open(filepath, 'wb') as f_out:
-        for idx in range(0, n_bytes, max_bytes):
-            f_out.write(bytes_out[idx:idx+max_bytes])
+    print(sys.platform)
+    if sys.platform == "darwin": # OS X
+        max_bytes = 2**31 - 1
+        bytes_out = pickle.dumps(obj)
+        n_bytes = sys.getsizeof(bytes_out)
+        with open(filepath, 'wb') as f_out:
+            for idx in range(0, n_bytes, max_bytes):
+                f_out.write(bytes_out[idx:idx+max_bytes])
+    else:
+        obj.to_pickle(filepath)
+
 
 
 def try_to_load_as_pickled_object_or_None(filepath):
     """
     This is a defensive way to write pickle.load, allowing for very large files on all platforms
     """
-    max_bytes = 2**31 - 1
-    try:
-        input_size = os.path.getsize(filepath)
-        bytes_in = bytearray(0)
-        with open(filepath, 'rb') as f_in:
-            for _ in range(0, input_size, max_bytes):
-                bytes_in += f_in.read(max_bytes)
-        obj = pickle.loads(bytes_in)
-    except:
-        return None
+    print(sys.platform)
+    if sys.platform == "darwin": # OS X
+        max_bytes = 2**31 - 1
+        try:
+            input_size = os.path.getsize(filepath)
+            bytes_in = bytearray(0)
+            with open(filepath, 'rb') as f_in:
+                for _ in range(0, input_size, max_bytes):
+                    bytes_in += f_in.read(max_bytes)
+            obj = pickle.loads(bytes_in)
+        except:
+            return None
+    else:
+        obj = pd.read_pickle(filepath)
+
     return obj
-
-
 
